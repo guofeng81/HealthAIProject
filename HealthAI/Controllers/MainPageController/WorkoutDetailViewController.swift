3363c022f5405f58c84341ab855d6733969dd5ee
Feng Guo <fguo81@gmail.com>
2018-11-14 09:45:46 -0600

    Complete reformat the workout app

--- HealthAI/Controllers/MainPageController/WorkoutDetailViewController.swift
+++ HealthAI/Controllers/MainPageController/WorkoutDetailViewController.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 
-class WorkoutDetailViewController: UIViewController {
+class WorkoutDetailViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,DataTransferDelegate {
+    
+    @IBOutlet var myTableView: UITableView!
     
     var selectedWorkoutItem = WorkoutItem()
+    
+    var selectedSubworkoutItem = SubworkoutItem()
+    var workoutHistoryItem = WorkoutHistoryItem()
+    
+    func userDidFinishedSubworkout(subworkoutItem: SubworkoutItem) {
+        selectedSubworkoutItem.done = subworkoutItem.done
+        //selectedSubworkoutItem.currentDate = subworkoutItem.currentDate
+        selectedSubworkoutItem.time = subworkoutItem.time
+        //selectedSubworkoutItem = subworkoutItem
+        print("Subworkout Item: ", subworkoutItem.done)
+        
+        //print("Selected Subworkout Item: ",selectedSubworkoutItem.done)
+        myTableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
     
     @IBOutlet weak var contentText: UILabel!
     @IBOutlet weak var titleText: UILabel!
@@ -21,16 +41,48 @@ class WorkoutDetailViewController: UIViewController {
         contentText.text = selectedWorkoutItem.content
     }
     
-    @IBAction func startWorkoutBtn(_ sender: Any) {
-        performSegue(withIdentifier: "beginWorkout", sender: self)
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedWorkoutItem.subworkouts.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subworkoutCell", for: indexPath)
+        
+        cell.textLabel!.text = selectedWorkoutItem.subworkouts[indexPath.row].title
+        
+        if selectedWorkoutItem.subworkouts[indexPath.row].done == true{
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "goToSubworkoutStopwatch", sender: self)
+        
+        selectedSubworkoutItem = selectedWorkoutItem.subworkouts[indexPath.row]
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        tableView.reloadData()
+        
+    }
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "beginWorkout" {
-            let seg = segue.destination as! SubWorkoutsViewController
-            seg.selectedWorkoutItem = self.selectedWorkoutItem
+        if segue.identifier == "goToSubworkoutStopwatch"{
+            let seg = segue.destination as! WorkoutClockViewController
+            seg.selectedSubworkoutItem = selectedSubworkoutItem
+            seg.delegate = self
         }
     }
+    
     
 
 }
