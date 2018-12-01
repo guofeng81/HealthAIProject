fa96d024c1a15e5ea323a59a9b9b97da77a25c47
Feng Guo <fguo81@gmail.com>
2018-12-01 02:12:18 -0600

    Complete setting up the app.

--- HealthAI/Controllers/MainPageController/VideosTableViewController.swift
+++ HealthAI/Controllers/MainPageController/VideosTableViewController.swift
@@ -12,7 +12,6 @@ import AVKit
 class VideosTableViewController: UITableViewController {
     
     
-
     let videos = ["BBC","BRC","DIC","CPC","ATD","CRP","CCGSR","CWGR","CWGLP","PO","BS","SM","BD","BMC","BBP","DFBF","DIBCP","DIBF","PU"]
     
     let videoTitles = ["Barbell Biceps Curl","Barbell Reverse Curl","Dumbbell Incline Curls","Cable Preacher Curls","Assisted Triceps Dip","Cable Rope Pushdowns","Cable Close Grip Seated Row","Cable Wide Grip Row","Cable Wide Grip Lat Pulldown","Pullover","Bodyweight Squat","Superman","Bench Dip","Bodyweigth Mountain Climbers","Barbell Bench Press","Dumbbell Flat Bench Fly","Dumbbell Incline Bench Chest Press","Dumbbell Incline Bench Fly","Push Up"]
@@ -21,22 +20,37 @@ class VideosTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return videos.count
+        return videoTitles.count
+        
+        
     }
-
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        cell.alpha = 0
+        let transform = CATransform3DTranslate(CATransform3DIdentity, -250, 20, 0)
+        cell.layer.transform = transform
+        
+        
+        UIView.animate(withDuration: 1.0) {
+            cell.alpha = 1.0
+            cell.layer.transform = CATransform3DIdentity
+        }
+    }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VideoCell", for: indexPath) as! VideoCell
@@ -49,7 +63,7 @@ class VideosTableViewController: UITableViewController {
         cell.playButton.tag = indexPath.row
         
         cell.playButton.addTarget(self, action: #selector(videoplayPressed(sender:)), for: .touchUpInside)
-
+        
         return cell
     }
     
@@ -87,8 +101,8 @@ class VideosTableViewController: UITableViewController {
                 
             })
         }
-    
+        
     }
-
-   
+    
 }
+
